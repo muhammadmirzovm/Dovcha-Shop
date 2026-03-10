406a6e797a41333476bdc81b8524c3f82f57cd79
mukhammadmirzo <mukhammadmirzo078@gmail.com>
2026-03-10 18:31:40 +0500

    Catalog , category, product-model, product-list

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ProductListView, ProductDetailView
+
+
+urlpatterns = [
+   path("", ProductListView.as_view(), name="product_list"),
+   path("products/<slug:slug>/", ProductDetailView.as_view(), name="product_detail"),
+]
+
+
--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
    path("admin/", admin.site.urls),
-   path("", home, name="home"),
+   # path("", home, name="home"),
    path("accounts/", include("accounts.urls")),
+   path("", include("catalog.urls"))
 ]
 
 if settings.DEBUG:
